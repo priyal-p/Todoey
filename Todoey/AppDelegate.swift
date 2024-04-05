c0bc9a87a2c708508bcc7189af719f581e1c9680
Priyal PORWAL <26priyalmrbd@gmail.com>
2024-04-05 14:06:33 +0530

    Refactoring

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print(Realm.Configuration.defaultConfiguration.fileURL ?? "")
 
         do {
-            let _ = try Realm()
+            _ = try Realm()
         } catch {
             print("Error initialising new realm", error)
         }
